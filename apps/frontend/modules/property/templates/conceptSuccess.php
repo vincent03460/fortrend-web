82dd618fda9001ac68787d3e4c9bd3fcb2bbc664
kleong153@gmail.com <kleong153@gmail.com@4529bf54-44f1-1adc-3b13-5b37c4b38157>
2015-01-20 04:41:10 +0000

    Correct CN translation.

--- apps/frontend/modules/property/templates/conceptSuccess.php
+++ apps/frontend/modules/property/templates/conceptSuccess.php
@@ -14,12 +14,9 @@ use_helper('I18N');
             <section class="col-lg-6 col-md-6 col-xs-12 col-sm-6 footer-one" style="margin-bottom:10px;">
                 <h3><?php echo __("Latest news & event") ?></h3>
                 <ul style="margin-left: 22px;">
-                    <li type="disc"><?php echo __("Environment conducive to talent and knowledge-based industry.") ?></li>
                     <li type="disc"><?php echo __("In the heart of Kuala Lumpur - minutes' walk to central busin ess district and PETRONAS Twin Towers.") ?></li>
-                    <li type="disc"><?php echo __("Pedestrian-friendly environment.") ?></li>
                     <li type="disc"><?php echo __("Public realm - open space for r ecreation and social activities.") ?></li>
                     <li type="disc"><?php echo __("Arts and cultural centre.") ?></li>
-                    <li type="disc"><?php echo __("Build-in future-proof digital master plan.") ?></li>
                     <li type="disc"><?php echo __("MRT accessibility and connectivity to major highways and airport.") ?></li>
                 </ul>
             </section>
